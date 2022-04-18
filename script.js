f849942f94f7319c0e23dbbd84e36c57c92c362a
Omar <otantawy53@gmail.com>
2022-04-18 19:13:24 -0400

    changed selfie alert to eventlistener function

--- script.js
+++ script.js
@@ -6,13 +6,35 @@ function hideAboutMe() {
   aboutMe.classList.toggle('hide');
 }
 
+const selfie = document.querySelector('img#selfie')
 let timesClicked = 0;
 
-function funnyAlert(){
+selfie.addEventListener('click', function (e) {
   timesClicked++;
   if (timesClicked === 1){
-    alert('Ouch, that hurt!');
-  } else if (timesClicked > 1){
-    alert('Please stop doing that');
-  }
-}
+    alert('Ouch, that hurt')};
+  if (timesClicked > 1){
+    alert('Please stop doing that')};
+})
+
+
+// let timesClicked = 0;
+//
+// function funnyAlert(){
+//   timesClicked++;
+//   if (timesClicked === 1){
+//     alert('Ouch, that hurt!');
+//   } else if (timesClicked > 1){
+//     alert('Please stop doing that');
+//   }
+// }
+
+
+// selfie.addEventListener('keydown', function (e) {
+//   if (event.key === "ArrowLeft"){
+//     const leftNumbers = selfie.style.left.replace('px', '');
+//     const left = parseInt(leftNumbers, 10);
+//
+//     selfie.style.left = `${left - 1}%`;
+//   }
+// });
