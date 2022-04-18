469557701a6afa341e7395ac988b6a1c2cb9ccb2
Omar <otantawy53@gmail.com>
2022-04-17 21:34:27 -0400

    got selfie alert to work

--- script.js
+++ script.js
@@ -6,40 +6,13 @@ function hideAboutMe() {
   aboutMe.classList.toggle('hide');
 }
 
-const selfie = document.querySelector('#selfie');
 let timesClicked = 0;
 
-// function funnyAlert (){
-//   let timesClicked = 0;
-//   while (timesClicked = 1){
-//     return alert('Ouch, that hurt.');
-//     timesClicked++;
-//     console.log(timesClicked);
-//   }
-// }
-
-
-function funnyAlert (){
-  while (timesClicked = 1){
-    return alert('Ouch, that hurt.');
-    timesClicked++;
-    console.log(timesClicked);
-  } if (timesClicked > 1){
-    return alert('Please stop hitting me.');
-    timesClicked++;
-    console.log(timesClicked);
+function funnyAlert(){
+  timesClicked++;
+  if (timesClicked === 1){
+    alert('Ouch, that hurt!');
+  } else if (timesClicked > 1){
+    alert('Please stop doing that');
   }
-  // timesClicked++;
-  // console.log(timesClicked);
 }
-
-
-// function funnyAlert (){
-//   if (timesClicked === 1){
-//     return alert('Ouch, that hurt.');
-//     timesClicked++;
-//   } else if (timesClicked > 1){
-//     return alert('Please stop doing that.');
-//   }
-//   timesClicked++;
-// }
